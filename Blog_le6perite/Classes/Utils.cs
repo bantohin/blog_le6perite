7a165d437b2cdfbe5ac5f75d48ec0d84ac3f866c
bantohin <dimitartachev23@gmail.com>
2016-08-12 16:12:41 +0300

    Mitio 1.0

---
+++ Blog_le6perite/Classes/Utils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Blog_le6perite.Classes
+{
+    public class Utils
+    {
+        public static string CutText(string text, int maxLength = 200)
+        {
+            if (text == null || text.Length <= maxLength)
+                return text;
+            var shortText = text.Substring(0, maxLength) + "...";
+            return shortText;
+        }
+    }
+}
