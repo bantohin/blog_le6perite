32ccb7db16966ca167736516499372337e36141c
dundurlunka <dundurlunka@abv.bg>
2016-08-12 13:07:52 +0300

    dundurlunka 1.0

--- Blog_le6perite/Controllers/HomeController.cs
+++ Blog_le6perite/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using Blog_le6perite.Models;
 
 namespace Blog_le6perite.Controllers
 {
     public class HomeController : Controller
     {
+        public ApplicationDbContext db = new ApplicationDbContext();
+
+        //GET:POST
         public ActionResult Index()
         {
-            return View();
+            var posts = db.Posts.Include(p => p.Author).OrderByDescending(p=>p.Date).Take(3);
+            return View(posts.ToList());
         }     
     }
 }
