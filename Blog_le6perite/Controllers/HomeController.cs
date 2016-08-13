590c3a495bc04d733bd8331e2d509813e43221df
bantohin <dimitartachev23@gmail.com>
2016-08-13 10:50:52 +0300

    Mitio 1.2

--- Blog_le6perite/Controllers/HomeController.cs
+++ Blog_le6perite/Controllers/HomeController.cs
@@ -16,6 +16,8 @@ namespace Blog_le6perite.Controllers
         public ActionResult Index()
         {
             var posts = db.Posts.Include(p => p.Author).OrderByDescending(p=>p.Date).Take(3);
+            var sidebarPosts = db.Posts.OrderByDescending(p => p.Date).Take(5);
+            ViewBag.SidebarPosts = sidebarPosts;
             return View(posts.ToList());
         }     
     }
