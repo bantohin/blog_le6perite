1c3c4c463364f322fd39fed16d62e9da670cbdcb
dundurlunka <dundurlunka@abv.bg>
2016-09-01 04:18:05 +0300

    dundurlunka 19.0
    Search added

---
+++ Blog_le6perite/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using Blog_le6perite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog_le6perite.Controllers
+{
+    public class SearchController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Search
+        public ActionResult Index()
+        {
+            var searchedWord = "";
+            searchedWord = Request.Form["searchEngine"];
+            var results = db.Posts.Select(p => p).Where(p => p.Title.Contains(searchedWord) || p.Author.FullName.Contains(searchedWord));
+            ViewBag.SearchResults = null;
+            if (results.Count() != 0)
+            {
+                ViewBag.SearchResults = results;
+            }
+            return View();
+        }
+    }
+}
